2892bc89fbbdd8b6867ceee3ff6338fb5c97b47d
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-12-02 18:49:45 -0500

    first commit

---
+++ movehat.config.ts
@@ -0,0 +1,8 @@
+export default {
+  network: "testnet",
+  profile: "default",
+  rpc: "https://testnet.movementnetwork.xyz/v1",
+  moveDir: "./move",         
+  account: "",
+  privateKey: "",
+};
